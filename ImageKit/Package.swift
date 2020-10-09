600c8007eaa5a054f1ad69804aed3ffc145361d9
Jing Liu <techliujing@gmail.com>
2020-10-10 00:06:13 +0800

    Update Package.swift
    修改Swift package类型

--- ImageKit/Package.swift
+++ ImageKit/Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "ImageKit",
-            type: .static,
+            type: .dynamic,
             targets: ["ImageKit"]),
     ],
     dependencies: [
